28a2ef7b4816992b0bceba04fe7c7e616836d0af
ArtemStarodubov <staroduboff.artem>
2024-03-27 13:54:34 +0400

    working with routes (just set up)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express =  require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv  from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,4 +12,5 @@ app.get("/", (req, res) => {
     res.send("Hello!!!");
 });
 
+app.use("/api/auth", authRoutes);
 app.listen(PORT, ()=> console.log(`Server Running on port ${PORT}`));
