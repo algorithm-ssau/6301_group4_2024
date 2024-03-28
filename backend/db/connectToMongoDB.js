8576d33a84b6c067a48354b404937a161c77ee36
alexkvereng <134049266+alexkvereng@users.noreply.github.com>
2024-03-28 10:09:36 +0400

    Create connectToMongoDB.js

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const connectToMongoDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_DB_URI);
+        console.log("Connect to MongoDB")
+
+    }
+    catch(error){
+        console.log(error.message);
+
+    }
+
+}
+export default connectToMongoDB;
