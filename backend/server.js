641dec4320a54ddacbe69eb5d90540c1f7f0c12b
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-04-14 22:37:22 +0400

    Added missing dependency on "connectToMongoDB.js"

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
