d9f1cb327ecfd5abe3cb31c15eed5f7cef6323ca
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-04-14 22:40:29 +0400

    The missing symbols ";" were added

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose';
 const connectToMongoDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_DB_URI);
-        console.log("Connect to MongoDB")
+        console.log("Connect to MongoDB");
     }
     catch (error) {
         console.log(error.message);
     }
-}
+};
 
 export default connectToMongoDB;
