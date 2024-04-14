1f2eccc5965cff507f1078cdaa54a59e8f7b5157
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-04-14 22:32:47 +0400

    The code was formatted

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
 import express from "express";
-import dotenv  from "dotenv";
+import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
-dotenv.config();
-app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
+dotenv.config();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 // app.get("/", (req, res) => {
-//     //root route http://localhost:5000/
+//     // root route http://localhost:5000/
 //     res.send("Hello!!!");
 // });
 
-app.use("/api/auth", authRoutes);
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     connectToMongoDB();
-    
-    console.log(`Server Running on port ${PORT}`)});
+    console.log(`Server Running on port ${PORT}`)
+});
