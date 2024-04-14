09517a81bcd82bbdefd647443834381cbd3a94eb
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-04-14 22:39:44 +0400

    The code was formatted

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
-const connectToMongoDB=async()=>{
-    try{
+
+const connectToMongoDB = async () => {
+    try {
         await mongoose.connect(process.env.MONGO_DB_URI);
         console.log("Connect to MongoDB")
-
     }
-    catch(error){
+    catch (error) {
         console.log(error.message);
-
     }
-
 }
+
 export default connectToMongoDB;
