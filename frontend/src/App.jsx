e555b487f14cd39277e7fbf1d0d1ec93a82187e4
ReppMikhail <mariareppchen@gmail.com>
2024-05-06 05:19:48 +0400

    Frontend: SignUp page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Login from './pages/login/Login';
+import SignUp from './pages/signup/SignUp';
 
 function App() {
   return <div className='p-4 g-screen flex items-center justify-center'>
