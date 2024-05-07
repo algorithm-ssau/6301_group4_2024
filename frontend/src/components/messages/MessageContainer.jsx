0e18a0b26fb903e73c4f6cb4278453c90e220a37
ArtemStarodubov <staroduboff.artem@yandex.ru>
2024-05-07 22:43:28 +0400

    nochatselected added

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Messages from './Messages';
+import MessageInput from './MessageInput';
 
 function MessageContainer() {
+  const noChatSelected = true;
   return (
     <div className='md:min-w-[450 px] flex flex-col'>
+      {noChatSelected? <NoChatSelected/> :(
         <>
         {/*Header*/}
         <div className='bg-stale-500 px-4 py-2 mb-2'>
@@ -14,8 +17,22 @@ function MessageContainer() {
         <Messages />
         <MessageInput />
         </>
+      )}
     </div>
   );
 };
 
 export default MessageContainer;
+
+
+const NoChatSelected =() =>{
+  return (
+    <div className='flex items-center justify-center w-full h-full'>
+      <div className='px-4 text-center sm:text-long md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+          <p>Welcome John Doe</p>
+          <p>Select a chat to start messaging</p>
+          <TiMessages className='text-3xl md:text-6xl text-center'/>
+      </div>
+    </div>
+  )
+}
