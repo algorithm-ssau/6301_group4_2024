97a0f9175b0d54a55d65b4ce70b9a32036048c6c
alexkvereng <134049266+alexkvereng@users.noreply.github.com>
2024-05-16 02:05:26 +0400

    Change for LogoutButton.jsx

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import {BiLogOut} from 'react-icons/bi';
 
 const LogoutButton = () => {
+  const {loading,logout}=useLogout()
   return (
     <div className='mt-auto'>
+      {!loading ? (
         <BiLogOut className='w-6 h-6 text-white cursor-pointer'/>
+      ):(<span className='loading loading-spinner'></span>)}
     </div>
   )
 }
