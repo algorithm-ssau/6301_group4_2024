94b8f6a2a7f01cb2504ddf514f86e1874f42d50c
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-05-24 02:07:14 +0400

    Text changed

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -3,7 +3,7 @@ const GenderCheckbox = ({onCheckboxChange,selectedGender}) => {
 		<div className='flex'>
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer ${selectedGender==="male" ? "selected":""}`}>
-					<span className='label-text'>Male</span>
+					<span className='label-text'>Мужчина</span>
 					<input type='checkbox' className='checkbox border-slate-900'
 					checked={selectedGender==="male"}
 					onChange={()=> onCheckboxChange("male")}
@@ -14,7 +14,7 @@ const GenderCheckbox = ({onCheckboxChange,selectedGender}) => {
 			</div>
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer ${selectedGender==="female" ? "selected":""}`}>
-					<span className='label-text'>Female</span>
+					<span className='label-text'>Женщина</span>
 					<input type='checkbox' className='checkbox border-slate-900'
 					checked={selectedGender==="female"}
 					onChange={()=> onCheckboxChange("female")}
