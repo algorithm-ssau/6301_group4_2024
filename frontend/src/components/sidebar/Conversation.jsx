375d5c5a67a74867b62f854e244d7f02df3cb848
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-05-24 15:08:25 +0400

    Text changed

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -10,7 +10,7 @@ const Conversation = () => {
         </div>
         <div className='flex flex-col flex-1'>
             <div className='flex gap-3 justify-between'>
-                <p className='font-bold text-gray-200'>John Doe</p>
+                <p className='font-bold text-gray-200'>Имя Фамилия</p>
                 <span className='text-xl'>✔</span>
             </div>
         </div>
