38d2a80c076e5ec878e90bb6139e71fa59443a8c
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-05-24 20:51:21 +0400

    Added brackets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,14 +9,14 @@ import {useAuthContext} from './context/AuthContext'
 import { Navigate } from 'react-router-dom';
 function App() {
   const {authUser}=useAuthContext()
-  return <div className='p-4 h-screen flex items-center justify-center'>
+  return (<div className='p-4 h-screen flex items-center justify-center'>
     <Routes>
       <Route path='/' element={authUser ? <Home/> : <Navigate to={"/login"}/>}/>
       <Route path='/login' element={authUser ? <Navigate to='/'/>:<Login/>}/>
       <Route path='/signup' element={authUser ? <Navigate to='/'/>:<Signup/>}/>
     </Routes>
     <Toaster/>
-  </div>;
+  </div>);
 }
 
 export default App;
