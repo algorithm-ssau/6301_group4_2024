cdf3fd227e8135b8211c486118d7aaa4e9ca0a7b
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-05-24 16:32:26 +0400

    Text changed

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -6,7 +6,7 @@ const MessageInput = () => {
         <div className='w-full relative'>
             <input type="text"
                 className='borader text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white'
-                placeholder='Send a message'
+                placeholder='Введите сообщение'
             />
             <button type='submit' className='absolute inset-y-0 end-0 flex items-center-pe-3'>
                 <BsSend/>
