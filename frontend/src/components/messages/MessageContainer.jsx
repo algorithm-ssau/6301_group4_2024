a52521ac829c436276185765a8ce4594b104882d
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-05-24 15:29:40 +0400

    Text changed

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -11,8 +11,8 @@ function MessageContainer() {
         <>
         {/*Header*/}
         <div className='bg-stale-500 px-4 py-2 mb-2'>
-            <span className='label-text'>To:</span>{" "}
-            <span className='text-gray-900 font-bold'> John Doe</span>
+            <span className='label-text'>Кому:</span>{" "}
+            <span className='text-gray-900 font-bold'> Имя Фамилия</span>
         </div>
 
         <Messages />
