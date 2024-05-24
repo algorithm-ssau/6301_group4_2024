cf4ab67fffc59c62b06dc5ad81a594ccd5f9c92f
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-05-24 15:51:54 +0400

    Text changed

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -13,8 +13,8 @@ const Message = () => {
             />
             </div>
         </div>
-        <div className={`chat-bubble text-white bg-blue-500`}> Hi! What is upp?</div>
-        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
+        <div className={`chat-bubble text-white bg-blue-500`}> Привет. Как дела?</div>
+        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>15:51</div>
     </div>
   )
 }
