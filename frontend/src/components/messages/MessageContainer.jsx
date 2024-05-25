d1b51ef61fadb9505f35a067f1ffe1c2dfdfa432
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-05-25 19:30:59 +0400

    Code has been changed

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Messages from './Messages';
 import MessageInput from './MessageInput';
 import { TiMessages } from "react-icons/ti";
+import useConversation from '../../zustand/useConversation';
 
 function MessageContainer() {
-  const noChatSelected = true;
+  const {selectedConversation, setSelectedConversation} = useConversation();
+  useEffect(() => {
+    // cleanup function (unmounts)
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
   return (
     <div className='md:min-w-[450 px] flex flex-col'>
-      {noChatSelected ? (<NoChatSelected />) : (
+      {!selectedConversation ? (<NoChatSelected />) : (
         <>
           {/*Header*/}
           <div className='bg-stale-500 px-4 py-2 mb-2'>
             {/* <span className='label-text'>Кому:</span>{" "} */}
-            <span className='text-gray-900 font-bold'> Имя Фамилия</span>
+            <span className='text-gray-900 font-bold'>{selectedConversation.fullname}</span>
           </div>
 
           <Messages />
