dbd5cbf78f32e934927b42ad35fb9d1c7eee313e
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-05-25 19:19:11 +0400

    Added event handler

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -8,8 +8,8 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
     return (
         <>
             <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
-            ${isSelected ? "bg-sky-500" : ""}
-            `}>
+            ${isSelected ? "bg-sky-500" : ""}`}
+            onClick={() => setSelectedConversation(conversation)}>
             <div className='avatar online'>
                 <div className='w-12 rounded-full'>
                     <img src={conversation.profilePic} alt="аватар пользователя" />
