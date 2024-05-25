3cd97ad0d4ee0449e138cc56a21097f7bb413440
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-05-25 14:03:39 +0400

    Added "useState" dependency

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,4 +1,6 @@
 import {Link} from "react-router-dom";
+import { useState } from "react";
+
 const Login = () => {
     const [username,setUsername]=useState("");
     const [password,setPassword]=useState("");
