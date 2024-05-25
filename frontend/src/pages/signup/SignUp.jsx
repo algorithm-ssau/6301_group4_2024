7ab0e40f1fa9171433a174b3dff411ca849c4e86
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-05-25 13:21:50 +0400

    Added dependencies

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import GenderCheckbox from "./GenderCheckbox";
+import useSignup from "../../hooks/useSignup";
 
 const SignUp = () => {
 	const [inputs,setInputs]= useState({
