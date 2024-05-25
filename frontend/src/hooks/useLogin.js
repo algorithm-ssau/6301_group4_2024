d85d9d2a4228a0ec1b6fc54c39a9fe9b37ea1787
Andrey Tofan <161709369+AndreyTofan@users.noreply.github.com>
2024-05-25 16:44:20 +0400

    Added "handleInputErrors" function and its call

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,34 +1,44 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { useAuthContext } from '../context/AuthContext';
 
-const useLogin=()=>{
-    const [loading,setLoading]=useState(false);
-    const {setAuthUser}=useAuthContext();
-    const login=async(username,password)=>{
+const useLogin = () => {
+    const [loading, setLoading] = useState(false);
+    const { setAuthUser } = useAuthContext();
+    const login = async (username, password) => {
+        const success = handleInputErrors(username, password);
+        if (!success) return;
         setLoading(true);
-        try{
-            const res=await fetch("api/auth/login",{
-                method:"POST",
-                headers:{"Content-Type":"application/json"},
-                body:JSON.stringify({username,password})
+        try {
+            const res = await fetch("api/auth/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, password })
             });
-            const data=await res.json();
-            if(data.error){
+            const data = await res.json();
+            if (data.error) {
                 throw new Error(data.error);
             }
-            localStorage.setItem("chat-user",JSON.stringify(data));
+            localStorage.setItem("chat-user", JSON.stringify(data));
             setAuthUser(data);
 
         }
-        catch(error){
+        catch (error) {
             toast.error(error.message);
 
         }
-        finally{
+        finally {
             setLoading(false);
         }
     };
-    return{loading, login};
+    return { loading, login };
 };
 export default useLogin;
+
+function handleInputErrors({username, password}){
+    if(!username || !password){
+        toast.error('Пожалуйста, заполните все поля');
+        return false;
+    }
+    return true;
+};
