dbeed41fee4d08725f38cca592c5c1fc96fd3372
DVMalkov <malkovd463@gmail.com>
2024-05-27 21:46:43 +0400

    starting to work on real-time messages

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -12,6 +12,11 @@ const io = new Server(server, {
 	},
 });
 
+
+export const getReceiverSocketId = (receiverId) => {
+	return userSocketMap[receiverId];
+};
+
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
