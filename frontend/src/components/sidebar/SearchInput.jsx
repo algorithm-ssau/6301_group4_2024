a4bb0d13da3c194aa6fc5e1604eb61b8ad49402e
ReppMikhail <mariareppchen@gmail.com>
2024-05-27 10:20:47 +0400

    добавлена обработка ввода handleSubmit

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
 
 const SearchInput = () => {
+    const [search, setSearch] = useState("");
+    const { setSelectedConversation } = useConversation();
+	const { conversations } = useGetConversations();
+
+    const handleSubmit = (e) => {}
     return (
-        <form className='flex items-center gap-2'>
-            <input type="text" placeholder='Поиск...' className='input input-bordered rounded-full' />
+        <form onSubmit={handleSubmit} className='flex items-center gap-2'>
+            <input type="text" placeholder='Поиск...' className='input input-bordered rounded-full'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            />
             <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
                 <IoSearchSharp className='w-6 h-6 outline-none' />
             </button>
